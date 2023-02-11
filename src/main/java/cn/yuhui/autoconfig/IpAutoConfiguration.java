957b5ba9dde8342aa82b4f7abf643b7496382536
yuhui <536430462@qq.com>
2023-02-11 20:54:03 +0800

    定时报表

--- src/main/java/cn/yuhui/autoconfig/IpAutoConfiguration.java
+++ src/main/java/cn/yuhui/autoconfig/IpAutoConfiguration.java
@@ -2,7 +2,9 @@ package cn.yuhui.autoconfig;
 
 import cn.yuhui.service.IpCountService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 public class IpAutoConfiguration {
     @Bean
     public IpCountService ipCountService(){
--- src/main/java/cn/yuhui/service/IpCountService.java
+++ src/main/java/cn/yuhui/service/IpCountService.java
@@ -1,5 +1,7 @@
 package cn.yuhui.service;
 
+import org.springframework.scheduling.annotation.Scheduled;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
@@ -16,12 +18,31 @@ public class IpCountService {
     @Resource
     private HttpServletRequest request;
 
-    public void count(){
+    public void count() {
         // 每次调用当前操作，就记录当前访问ip，累加访问次数
         // 1、获取访问者ip
         String ip = request.getRemoteAddr();
         System.out.println("--------------" + ip);
         // 2、根据ip从map取值并递增（如果第一次访问，给默认值1）
         ipCountMap.put(ip, ipCountMap.getOrDefault(ip, 0) + 1);
+    }
+
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void print() {
+        System.out.println("         IP访问监控");
+        System.out.println("+-----ip-address-----+--num--+");
+        for (Map.Entry<String, Integer> entry : ipCountMap.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            System.out.println(String.format("|%18s  |%5d  |", key, value));
+        }
+        System.out.println("+--------------------+-------+");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("         IP访问监控");
+        System.out.println("+-----ip-address-----+--num--+");
+        System.out.println(String.format("|%18s  |%5d  |", "abc", 123));
+        System.out.println("+--------------------+-------+");
     }
 }
